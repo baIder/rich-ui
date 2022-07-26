14cd4c9108de214b82fe5da8d2033dff5ecfbec3
baIder <511763880@qq.com>
2022-07-26 19:39:14 +0800

    Switch 可以接受props

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <Switch/>
+    <Switch :value="y" @input="y=$event"/>
   </div>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 
 export default {
   name: 'SwitchDemo',
-  components: {Switch}
+  components: {Switch},
+  setup() {
+    const y = ref(true);
+    return {y};
+  }
 };
 </script>
 
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,18 +1,20 @@
 <template>
-  <button :class="{checked}" @click="toggle"><span></span></button>
+  <button :class="{checked:value}" @click="toggle"><span></span></button>
+  <div>{{ value }}</div>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
 
 export default {
   name: 'Switch',
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit('input', !props.value);
     };
-    return {checked, toggle};
+    return {toggle};
   }
 };
 </script>
