8c7c679fea25e9ae151fa22cdcff39cdf1b543f2
baIder <511763880@qq.com>
2022-07-26 18:59:11 +0800

    路由切换后关闭aside

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>SwitchDemo</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SwitchDemo'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router.ts
@@ -0,0 +1,16 @@
+import {createWebHashHistory, createRouter} from 'vue-router';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import SwitchDemo from './components/SwitchDemo.vue';
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history, routes: [
+    {path: '/', component: Home},
+    {
+      path: '/doc', component: Doc, children: [
+        {path: 'switch', component: SwitchDemo}
+      ]
+    },
+  ]
+});
