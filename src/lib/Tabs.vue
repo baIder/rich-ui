bcf14bc64d20d3dc8b7e66d0fd9a8d3e17d2b267
baIder <511763880@qq.com>
2022-07-27 17:21:10 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="rich-tabs">
-    <div class="rich-tabs-nav">
-      <div v-for="(t,index) in titles" :key="index" :class="{selected:t===selected}" class="rich-tabs-nav-item"
-           @click="select(t)">{{
-          t
-        }}
+    <div ref="container" class="rich-tabs-nav">
+      <div v-for="(t,index) in titles" :key="index" :ref="el=>{if(el) navItems[index] = el}"
+           :class="{selected:t===selected}"
+           class="rich-tabs-nav-item" @click="select(t)">{{ t }}
       </div>
+      <div ref="indicator" class="rich-tabs-nav-indicator"></div>
     </div>
     <div class="rich-tabs-content">
-      <component :is="current" :key="selected" class="rich-tabs-content-item"/>
+      <component :is="current" :key="current.props.title" class="rich-tabs-content-item"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {computed} from "vue";
+import {computed, onMounted, onUpdated, ref} from "vue";
 
 export default {
   name: 'Tabs',
@@ -25,6 +25,21 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {left: left1} = container.value.getBoundingClientRect()
+      const {left: left2} = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default();
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -42,7 +57,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {defaults, titles, current, select};
+    return {defaults, titles, current, select, navItems, indicator, container};
   }
 };
 </script>
@@ -56,6 +71,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -69,6 +85,18 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+
+
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
 
