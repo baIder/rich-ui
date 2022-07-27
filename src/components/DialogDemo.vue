ba3954b24b5f792aa4c081ca155381c9af30e50f
baIder <511763880@qq.com>
2022-07-27 14:55:10 +0800

    实现visibl

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,16 +1,27 @@
 <template>
   <div>DialogDemo</div>
   <h1>示例1</h1>
-  <Dialog/>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x"/>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
   name: 'DialogDemo',
-  components: {Dialog},
+  components: {Button, Dialog},
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    return {x, toggle};
+  }
 };
+
 </script>
 
 <style lang="scss" scoped>
