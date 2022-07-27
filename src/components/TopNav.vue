2246543f561c55598f9bdd90305b21ce35b4e48a
baIder <511763880@qq.com>
2022-07-27 18:08:47 +0800

    背景渐变

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -28,7 +28,6 @@ export default {
 
 <style lang="scss" scoped>
 .topNav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topNavAndBanner">
     <TopNav/>
     <div class="banner">
       <h1>发财 UI </h1>
@@ -22,13 +22,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.topNavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
 
   > .actions {
     padding: 8px 0;
