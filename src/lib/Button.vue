e8ffbe8a6ecc8a0e5e91a14327954b63ec74e374
baIder <511763880@qq.com>
2022-07-27 13:57:51 +0800

    调整CSS支持theme

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -54,5 +54,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.rich-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.rich-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
