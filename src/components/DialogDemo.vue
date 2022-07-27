eb734b38ac21587ba3e1e08fa2ed6bf134a0ca73
baIder <511763880@qq.com>
2022-07-27 14:48:53 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
   <div>DialogDemo</div>
+  <h1>示例1</h1>
+  <Dialog/>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
 export default {
-  name: 'DialogDemo'
+  name: 'DialogDemo',
+  components: {Dialog},
 };
 </script>
 
