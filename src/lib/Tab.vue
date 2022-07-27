eb836506a23bb53e6de2e0b396319bc6f6246935
baIder <511763880@qq.com>
2022-07-27 15:53:08 +0800

    创建Tabs组件

---
+++ src/lib/Tab.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Tab组件</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Tab'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Tabs组件
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Tabs'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
