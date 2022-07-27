26d88975ffc33b6e9bf28a46951d6fb19a483fc1
baIder <511763880@qq.com>
2022-07-27 13:02:15 +0800

    创建Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,16 @@
 <template>
-<div>ButtonDemo</div>
+  <div>ButtonDemo</div>
+  <h1>示例1</h1>
+  <Button>你好</Button>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue'
+
 export default {
-  name: 'ButtonDemo'
+  name: 'ButtonDemo',
+  components: {Button}
+
 };
 </script>
 
