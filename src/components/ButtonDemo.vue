a8ffa4d7c9d2a5822ecb73e433b750f3527dcf56
baIder <511763880@qq.com>
2022-07-27 14:08:54 +0800

    支持theme

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -21,6 +21,24 @@
     <Button theme="text">普普通</Button>
     <Button size="small" theme="text">小小小</Button>
   </div>
+  <h1>示例3</h1>
+  <div>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button level="main" theme="link">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button level="danger" theme="link">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button level="main" theme="text">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button level="danger" theme="text">危险文字按钮</Button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,13 +18,18 @@ export default {
       type: String,
       default: 'normal'
     },
+    level: {
+      type: String,
+      default: 'normal',
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`rich-theme-${theme}`]: theme,
         [`rich-size-${size}`]: size,
+        [`rich-level-${level}`]: level,
       };
     });
     return {classes};
@@ -38,6 +43,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .rich-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -103,5 +110,61 @@ $radius: 4px;
     padding: 0 4px;
   }
 
+  &.rich-theme-button {
+    &.rich-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.rich-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.rich-theme-link {
+    &.rich-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.rich-theme-text {
+    &.rich-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.rich-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style>
