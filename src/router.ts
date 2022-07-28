8fa06a7fe963f642a1e5bfcf995eb3f276ea03f0
baIder <511763880@qq.com>
2022-07-28 17:49:01 +0800

    解决build后md文件报错

--- src/router.ts
+++ src/router.ts
@@ -6,19 +6,22 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import Markdown from "./components/Markdown.vue";
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 import {h} from "vue";
 
 const history = createWebHashHistory();
-const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename})
+const md = string => h(Markdown, {content: string, key: string})
 export const router = createRouter({
     history, routes: [
         {path: '/', component: Home},
         {
             path: '/doc', component: Doc, children: [
                 {path: '', redirect: '/doc/intro'},
-                {path: 'intro', component: md('intro')},
-                {path: 'install', component: md('install')},
-                {path: 'get-started', component: md('get-started')},
+                {path: 'intro', component: md(intro)},
+                {path: 'install', component: md(install)},
+                {path: 'get-started', component: md(getStarted)},
                 {path: 'switch', component: SwitchDemo},
                 {path: 'button', component: ButtonDemo},
                 {path: 'dialog', component: DialogDemo},
