0f5d87b889d46674aa50dc4f0ff1848185a10b6c
baIder <511763880@qq.com>
2022-07-28 16:20:51 +0800

    展示Switch源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>{{ Switch1Demo.__sourceCode }}</pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
       </div>
     </div>
   </div>
@@ -40,7 +40,7 @@ export default {
   components: {Switch, Button, Switch1Demo, Switch2Demo},
   setup() {
     const bool = ref(true);
-    return {bool};
+    return {bool, Switch1Demo, Switch2Demo};
   }
 };
 </script>
