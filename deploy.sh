418c706c972c843aa6b68610c2b4f3990a8e120a
baIder <511763880@qq.com>
2022-07-28 18:11:04 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ rm -rf dist
 yarn build
 git init
 git add .
-git commit -m 'init'
+git commit -m 'update'
 git remote add origin git@github.com:baIder/rich-ui-website.git
 git branch -M main
 git push -f -u origin main
