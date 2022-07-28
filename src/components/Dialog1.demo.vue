f724fc0eeeb52ebbebd3b1f436074c981bc330a4
baIder <511763880@qq.com>
2022-07-28 17:16:23 +0800

    改造DialogDemo

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,53 @@
+<demo>
+常规使用
+</demo>
+<template>
+  <div>
+    <Button @click="toggle">打开对话框</Button>
+    <Dialog v-model:visible="x" :cancel="f2" :closeOnClickOverlay="false" :ok="f1">
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue'
+
+
+export default {
+  name: "Dialog1.demo",
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {
+    }
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,45 +1,22 @@
 <template>
-  <div>DialogDemo</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :cancel="f2" :close-on-click-overlay="false" :ok="f1">
-    <template v-slot:title>
-      <strong>加粗的提示</strong>
-    </template>
-    <template v-slot:content>
-      <strong>内容1</strong>
-      <div>内容2</div>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-  <Button @click="showDialog">show</Button>
+  <h1>Dialog示例</h1>
+  <Demo :component="Dialog1Demo"/>
+  <Demo :component="Dialog2Demo"/>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
-import {openDialog} from '../lib/openDialog';
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1.demo.vue';
+import Dialog2Demo from './Dialog2.demo.vue';
 
 export default {
   name: 'DialogDemo',
-  components: {Button, Dialog},
+  components: {Demo},
   setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
+    return {
+      Dialog1Demo,
+      Dialog2Demo
     };
-    const f1 = () => {
-      console.log(1);
-      return false;
-    };
-    const f2 = () => {
-      console.log(2);
-    };
-    const showDialog = () => {
-      openDialog({title: '标题', content: '你好', ok() {console.log('OK');}, cancel() {console.log('cancel');}});
-    };
-    return {x, toggle, f1, f2, showDialog};
   }
 };
 
