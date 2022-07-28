af401a66bc4eeadc5fdd0e6bd2e317bdc93b05f1
baIder <511763880@qq.com>
2022-07-28 17:03:12 +0800

    优化Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,10 +5,10 @@
       <component :is="component"/>
     </div>
     <div class="demo-actions">
-      <Button>查看代码</Button>
+      <Button @click="codeVisible=!codeVisible">查看代码</Button>
     </div>
-    <div class="demo-code">
-      <pre class="language-html" v-html=" Prism.highlight(component.__sourceCode, Prism.languages.html, 'html') "/>
+    <div v-if="codeVisible" class="demo-code">
+      <pre class="language-html" v-html="html"/>
     </div>
   </div>
 </template>
@@ -17,16 +17,22 @@
 import Button from '../lib/Button.vue'
 import 'prismjs'
 import 'prismjs/themes/prism.css'
+import {computed, ref} from "vue";
 
 const Prism = (window as any).Prism
 
 export default {
   name: "Demo",
+  components: {Button},
   props: {
-    component: Object, Button
+    component: Object
   },
-  setup() {
-    return {Prism}
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+    })
+    let codeVisible = ref(false)
+    return {Prism, html, codeVisible}
   }
 }
 </script>
