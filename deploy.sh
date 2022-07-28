0e08065ba3e1decc2908eb4f0362ec64f526d45f
baIder <511763880@qq.com>
2022-07-28 18:56:50 +0800

    deploy.sh 小调整

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-rm -rf dist
-yarn build
-cd dist
-git init
-git add .
-git commit -m 'update'
-git remote add origin git@github.com:baIder/rich-ui-website.git
-git branch -M main
-git push -f -u origin main
+rm -rf dist &&
+yarn build &&
+cd dist  &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git remote add origin git@github.com:baIder/rich-ui-website.git &&
+git branch -M main &&
+git push -f -u origin main &&
 cd -
+echo https://git.balder.wang/rich-ui-website/
