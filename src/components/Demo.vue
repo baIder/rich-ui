93b7ad5ef0349d945eba726bd399cdb109fa62f2
baIder <511763880@qq.com>
2022-07-28 19:09:14 +0800

    隐藏按钮，显示按钮

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component"/>
     </div>
     <div class="demo-actions">
-      <Button @click="codeVisible=!codeVisible">查看代码</Button>
+      <Button v-if="codeVisible" @click="hideCode">隐藏代码</Button>
+      <Button v-else @click="showCode">查看代码</Button>
     </div>
     <div v-if="codeVisible" class="demo-code">
       <pre class="language-html" v-html="html"/>
@@ -32,7 +33,9 @@ export default {
       return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
     })
     let codeVisible = ref(false)
-    return {Prism, html, codeVisible}
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
+    return {Prism, html, codeVisible, showCode, hideCode}
   }
 }
 </script>
