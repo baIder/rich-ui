35796a4380547e7f41b99b76f576d3cb4d28034a
baIder <511763880@qq.com>
2022-08-01 01:09:08 +0800

    引入字体 增加渐入的加载动画

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {createApp} from 'vue';
 import './lib/rich.scss';
 import './index.scss';
+import './assets/page-loading.scss'
 import App from './App.vue';
 import {router} from './router';
 import 'github-markdown-css'
@@ -9,3 +10,15 @@ import 'github-markdown-css'
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+
+window.onload = () => {
+    setTimeout(() => {
+        const loadingWrapper = document.getElementById('loading-wrapper')
+        loadingWrapper.classList.remove('page-loading')
+        loadingWrapper.ontransitionend = () => {
+            loadingWrapper.remove()
+        }
+        console.log('加载完成JS')
+    }, 100)
+
+}
