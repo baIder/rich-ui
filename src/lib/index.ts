8767707406d727c32df25d0d2a709a8aafebbb17
baIder <511763880@qq.com>
2022-08-01 13:17:12 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+export {default as Switch} from './Switch.vue';
+export {default as Button} from './Button.vue';
+export {default as Tabs} from './Tabs.vue';
+export {default as Tab} from './Tab.vue';
+export {default as Dialog} from './Dialog.vue';
+export {default as Message} from './Message.vue';
+export {openDialog as openDialog} from './openDialog';
+export {popMessage as popMessage} from './popMessage';
